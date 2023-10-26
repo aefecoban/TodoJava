42ba9a4871a155025ad51ae0d277bf41e6227d19
Efe <61231517+aefecoban@users.noreply.github.com>
2023-10-26 03:22:52 +0300

    Create UserInfos.java

---
+++ src/Models/UserInfos.java
@@ -0,0 +1,66 @@
+package Models;
+
+import java.util.ArrayList;
+
+public class UserInfos {
+    
+    ArrayList<UserInfo> infos;
+
+    public UserInfos() {
+        this(new ArrayList<UserInfo>());
+    }
+
+    public UserInfos(ArrayList<UserInfo> infoFromOutside) {
+        infos = infoFromOutside;
+    }
+
+    public int size(){
+        return infos.size();
+    }
+
+    public void Add(UserInfo ui){
+        if(!this.infos.contains(ui))
+            this.infos.add(ui);
+    }
+
+    public void Remove(UserInfo ui){
+        if(this.infos.contains(ui))
+            this.infos.remove(ui);
+    }
+
+    public void Remove(int ID){
+        if(ID < this.infos.size())
+            this.infos.remove(ID);
+    }
+
+    public UserInfo Get(int ID){
+        return this.infos.get(ID);
+    }
+
+    public UserInfo Get(String Title){
+        for (UserInfo userInfo : infos) {
+            if(userInfo.Title == Title){
+                return userInfo;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<UserInfo> Search(String SearchText){
+        ArrayList<UserInfo> results = new ArrayList<UserInfo>();
+
+        for (UserInfo userInfo : infos) {
+            if(
+                userInfo.Title.contains(SearchText) ||
+                userInfo.Username.contains(SearchText) ||
+                userInfo.Email.contains(SearchText) ||
+                userInfo.Password.contains(SearchText) ||
+                userInfo.Desc.contains(SearchText)
+            )
+                results.add(userInfo);
+        }
+
+        return results;
+    }
+
+}
