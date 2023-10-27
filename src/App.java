7920b2a027025f2634e27fe7ce7534fac67e5902
Efe <61231517+aefecoban@users.noreply.github.com>
2023-10-27 03:42:38 +0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,19 +1,9 @@
-import Models.UserInfo;
-import Models.UserInfos;
-
 public class App {
 
     public static void main(String[] args) {
 
-        UserInfo UI1 = new UserInfo("Web Site", "user", "123456");
-        UserInfo UI2 = new UserInfo("Mobile Application", "mobil_user", "123456");
-        UserInfo UI3 = new UserInfo("Game", "gamer", "123456");
-
-        UserInfos Database = new UserInfos();
-        Database.Add(UI1);
-        Database.Add(UI1);
-        Database.Add(UI2);
-        Database.Add(UI3);
+        Application application = new Application();
+        application.Run();
 
     }
 
